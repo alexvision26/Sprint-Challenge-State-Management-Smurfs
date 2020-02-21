cc72b67a2d1234ae922f1c6a1b13eb089ab49e2b
Alex Miller <alexmillerprojects@gmail.com>
2020-02-21 08:15:42 -0800

    initial push, set up file structure

---
+++ smurfs/src/components/SmurfsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const SmurfsList = props => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SmurfsList;
