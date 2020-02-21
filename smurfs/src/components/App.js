3fa3c13f13cc95fe3828f2dc97a8cd2ff65364d9
Alex Miller <alexmillerprojects@gmail.com>
2020-02-21 10:55:51 -0800

    finished mvp and stretch goal

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -4,13 +4,15 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className='overlay'>
+      <>
+      
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
         <SmurfsList />
       </div>
-      </div>
+      <div className='overlay'></div>
+      </>
     );
   }
 }
